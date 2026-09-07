7c0c3aef1742436b858d5a66aea69fb46a65b878
Reyes Sánchez Laura Pamela <pamelasanz95@gmail.com>
2026-09-06 22:44:31 -0600

    Cuarto cubo

--- ConfigInicial/Main_Practica3.cpp
+++ ConfigInicial/Main_Practica3.cpp
@@ -217,7 +217,18 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		//CUarto cubo - Letra R
+		model = glm::mat4(2);
+		model = glm::translate(model, glm::vec3(-11.5f, 6.0f, -10.0f));
+		model = glm::rotate(model, 9.5f, glm::vec3(-2.0f, -16.0f, 15.0f));
+		model = glm::scale(model, glm::vec3(6.0f, 6.0f, 6.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
+		glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
 
